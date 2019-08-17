fc439e79ab711883889ae5c29f6f2a7b08681bff
Sebastian Chavez <seba.chavez21@gmail.com>
2019-08-17 12:52:57 -0400

    primer commit

--- src/pages/login.js
+++ src/pages/login.js
@@ -2,35 +2,47 @@ import React, {Component} from 'react';
 import {
   StyleSheet,
   View,
-  ImageBackground,
   Text,
   TextInput,
   Dimensions,
-  Icon
+  TouchableOpacity,
+  Image
 } from 'react-native';
-
-import bgImage from '../assets/img/fondo.jpg'
+// import Svg, {G, Path} from 'react-native-svg '
 
 const { width: WIDTH } = Dimensions.get('window')
 class Login extends Component {
+
   render(){
     return (
-     <ImageBackground source={bgImage} style={styles.backgroundContainer}>
-       <View style={styles.logoContainer}>
-         {/* <Image source={logo} style={styles.logo} /> */}
-         <Text style={styles.logoText}>Easy Cook</Text>
-       </View>
        <View>
-         <Icon name={'person'} size={28} color={'rgba(255, 255, 255, 0.7)'} 
-         style={styles.inputIcon}/>
-         <TextInput 
-          style={styles.input}
-          placeholder={'email'}
-          placeholderTextColor={'rgba(255, 255, 255, 0.7)'}
-          underlineColorAndroid='transparent'
-         />
+        <View style={styles.logoContainer}>
+          {/* <Image source={logo} style={styles.logo} /> */}
+          <Text style={styles.logoText}>Easy Cook</Text>
+        </View>
+        <View style={styles.inputContainer}>
+          {/* <FontAwesome name='users' size={28} color={'rgba(255, 255, 255, 0.7)'} 
+          style={styles.inputIcon}/> */}
+          <TextInput 
+            style={styles.input}
+            placeholder={'email'}
+            placeholderTextColor={'rgba(255, 255, 255, 0.7)'}
+            underlineColorAndroid='transparent'
+          />
+        </View>
+        <View style={styles.inputContainer}>
+        <TextInput 
+            style={styles.input}
+            secureTextEntry={true}
+            placeholder={'contraseña'}
+            placeholderTextColor={'rgba(255, 255, 255, 0.7)'}
+            underlineColorAndroid='transparent'
+          />
+        <TouchableOpacity style={styles.btnEye}>
+          <Image  source={this.props.iconView}/>
+        </TouchableOpacity>
+        </View>
        </View>
-     </ImageBackground>
     )
   }
 };
@@ -38,14 +50,6 @@ class Login extends Component {
 
 
 const styles = StyleSheet.create({
-  backgroundContainer: {
-    flex: 1,
-    width: null,
-    height: null,
-    justifyContent: 'center',
-    alignItems: 'center',
-    // backgroundColor: '#F5FCFF' 
-  },
   logo: {
     width: 120,
     height: 120,
@@ -74,6 +78,14 @@ const styles = StyleSheet.create({
     position:'absolute',
     top: 8,
     left: 37
+  },
+  inputContainer: {
+    marginTop: 10
+  },
+  btnEye: {
+    position:'absolute',
+    top: 8,
+    right: 37
   }
 });
 
